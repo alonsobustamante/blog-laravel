faf7fe5aa8c980a42288881f4684a8e29ef4148f
Alonso Bustamante <carlosaj.bustamante@gmail.com>
2022-05-01 23:46:15 -0500

    Se cambio el nombre del logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -45,7 +45,7 @@ return [
     |
     */
 
-    'logo' => '<b>Coders</b>FREE',
+    'logo' => '<b>ElOso</b>JUDIO',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
